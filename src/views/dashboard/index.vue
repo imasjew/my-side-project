603e744f8f0b4eaa1d148f43654e6265c97c964e
imasjew <yif@http://192.168.1.10:10082/RD/frontend-dev-website.git.com>
2022-04-04 08:34:35 +0800

    登录页

---
+++ src/views/dashboard/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="login-wrap">
+    dashboard
+		{{ slogan}}
+  </div>
+</template>
+<script setup>
+const slogan = 'sss'
+</script>
+<style lang="less">
+.login-wrap {
+  color: red;
+}
+</style>
